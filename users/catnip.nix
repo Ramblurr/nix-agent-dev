5e284e2014306daf4fa35d36f2e22dfa07650c12
Casey Link <unnamedrambler@gmail.com>
2025-12-18 21:52:18 +0100

    add catnip user

---
+++ users/catnip.nix
@@ -0,0 +1,23 @@
+# This returns homeManagerConfiguration args for flakelight
+# Note: flakelight passes inputs to the home-manager configuration via extraSpecialArgs
+_: {
+  system = "x86_64-linux";
+  modules = [
+    (
+      {
+        pkgs,
+        lib,
+        inputs,
+        ...
+      }:
+      {
+        imports = [
+          ../config/home.nix
+        ];
+
+        home.username = "catnip";
+        home.homeDirectory = "/home/catnip";
+      }
+    )
+  ];
+}
