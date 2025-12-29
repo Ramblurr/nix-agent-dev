0e5d3b442fecaf2b52ff35835d83d8d3795da77e
Casey Link <unnamedrambler@gmail.com>
2025-12-29 09:04:19 +0100

    update clojure template

--- templates/clojure/dev/user.clj
+++ templates/clojure/dev/user.clj
@@ -1,24 +1,22 @@
 (ns user
   (:require
-   [portal.api :as p]
-   [clj-reload.core :as clj-reload]
-   [ol.dev.portal :as my-portal]))
+   #_[portal.api :as p]
+   [clj-reload.core :as clj-reload]))
 
 ((requiring-resolve 'hashp.install/install!))
 
 (set! *warn-on-reflection* true)
-(set! *print-namespace-maps* false)
 
 ;; Configure the paths containing clojure sources we want clj-reload to reload
 (clj-reload/init {:dirs      ["src" "dev" "test"]
                   :no-reload #{'user 'dev 'ol.dev.portal}})
 
-(defonce ps (my-portal/open-portals))
-
 (comment
+  (defonce ps ((requiring-resolve 'ol.dev.portal/open-portals)))
+
   (clj-reload/reload)
   (clj-reload/reload {:only :all}) ;; rcf
-  (reset! my-portal/portal-state nil)
+  #_(reset! my-portal/portal-state nil)
   (clojure.repl.deps/sync-deps)
   ;;;
   )
